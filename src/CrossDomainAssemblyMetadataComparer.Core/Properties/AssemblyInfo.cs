4eb17a435babae08cec61a6e71bd73296ac74a7c
vmcl <none@none>
2016-11-08 12:34:30 -0500

    CrossDomainAssemblyMetadataComparer project skeleton

---
+++ src/CrossDomainAssemblyMetadataComparer.Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CrossDomainAssemblyMetadataComparer.Core")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Cross Domain Assembly Metadata Comparer")]
+[assembly: AssemblyCopyright("Copyright © Vitalii Maklai 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("6d24263a-8a40-4936-b1e3-93f83e10cbef")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
