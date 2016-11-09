91ba50eacf50426b829a532962707c73bc68c135
vmcl <none@none>
2016-11-09 18:13:43 -0500

    Set the assembly version to 0.1 (except test assemblies)

--- src/CrossDomainAssemblyMetadataComparer.Core/Properties/AssemblyInfo.cs
+++ src/CrossDomainAssemblyMetadataComparer.Core/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("6d24263a-8a40-4936-b1e3-93f83e10cbef")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
